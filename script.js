710ab4f6a01a6f8c956c59f9afbc3c5b81653c75
florenceafo <florence.afolabi@gmail.com>
2018-11-21 00:52:59 +0100

    script parses JSON file and prints data to HTML file

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
+const key = "f706430bae10de65bf9eacb0eeb77df9";
+var showCity = document.getElementById("city");
 function getWeather() {
     navigator.geolocation.getCurrentPosition(function(position) {
-        var lat = position.coords.latitude;
+        var lat = Math.round(position.coords.latitude * 10) / 10;
         var long = position.coords.longitude;
-        var api = "http://api.openweathermap.org/data/2.5/find?lat=" + lat + "&lon=" + long + "&cnt=10";
+        var url = "http://api.openweathermap.org/data/2.5/find?lat=" + lat + "&lon=" + long + "&cnt=1&appid=" + key;
         console.log(lat);
 	    console.log(long);
+        console.log(url);
 
+        getWeatherData(url);
     });
+}
+
+function getWeatherData(url) {
+    var request = new XMLHttpRequest();
+    console.log(url);
+    request.open('GET', url);
+    request.onload = function() {
+        var x = JSON.parse(request.responseText);
+        addHTML(x);
+
+    }
+    request.send(null);
+}
+
+function addHTML(x) {
+    showCity.innerHTML = "";
+    var getCity = "<p>" + x.list[0].name + "</p>";
+    showCity.innerHTML = getCity;
 }
